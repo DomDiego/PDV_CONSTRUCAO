fba05a2cb6888b74fe9c5804b9dec017a64579a8
Diego Dos Santos <diego.axiole@yahoo.com.br>
2025-09-30 13:08:44 -0300

    adicionado o console log ao main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// DEBUG das variáveis de ambiente
+console.log('ENV:', import.meta.env)
+console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL)
+console.log('Supabase Key:', import.meta.env.VITE_SUPABASE_ANON_KEY ? 'OK' : 'MISSING')
+
 const app = createApp(App)
 
 app.use(createPinia())
