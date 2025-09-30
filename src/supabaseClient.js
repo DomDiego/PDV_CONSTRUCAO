ea1953289bdc0088fc288eb981471f26d25049b1
Diego Dos Santos <diego.axiole@yahoo.com.br>
2025-09-30 13:34:38 -0300

    chave inserida direto no supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://dzqauwawbtxcdpouogyl.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cWF1d2F3YnR4Y2Rwb3VvZ3lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNjc1NzEsImV4cCI6MjA3NDc0MzU3MX0.PsBKfNbta2HWQHw-Y-H3r4sRIhjfB3BNo3iQpYi-uAw"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
