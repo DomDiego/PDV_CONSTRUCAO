be1fc615d03c6e2488e02fd67c7a90bb15eae36f
Diego Dos Santos <diego.axiole@yahoo.com.br>
2025-09-30 12:53:09 -0300

    alteração nos arquivos supabasecliente.js e .env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,11 +1,7 @@
 // src/supabaseClient.js
-
 import { createClient } from '@supabase/supabase-js'
 
-// ESTA LINHA É A CRUCIAL! Deve ter DATABASE_URL
-const supabaseUrl = import.meta.env.VITE_SUPABASE_DATABASE_URL
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-// ... (Resto do seu código)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
