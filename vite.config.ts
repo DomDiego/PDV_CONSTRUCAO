bbfab84772e4d4769b1615acf8014312cf87d3a4
Diego Dos Santos <diego.axiole@yahoo.com.br>
2025-09-30 12:37:56 -0300

    fix: Adiciona base path para deploy estatico no Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // ADICIONE ESTA LINHA CRUCIAL!
+  base: './',
+
   plugins: [
     vue(),
     vueDevTools(),
